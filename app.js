b4c10f0782cf39f05a2054d0666b7eab032ee59d
shankarvrp1 <50736046+shankarvrp1@users.noreply.github.com>
2019-07-25 18:06:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var env = process.env.NODE_ENV || 'development';
 app.locals.ENV = env;
 app.locals.ENV_DEVELOPMENT = env == 'development';
 
-// view engine setup
+// view engine setup completed
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
